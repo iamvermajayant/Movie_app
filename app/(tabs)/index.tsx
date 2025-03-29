f5497421e831809f4a7923627be4ff24c759dfdc
Jayant Verma <iamvermajayant@gmail.com>
2025-03-29 22:49:08 +0530

    created a custom hook useFetch made api call in index component for latest movies

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,11 +1,15 @@
 import { icons } from "@/constants/icons";
 import { images } from "@/constants/images";
-import { Image, ScrollView, Text, View } from "react-native";
+import { ActivityIndicator, FlatList, FlatListComponent, Image, ScrollView, Text, View } from "react-native";
 import SearchBar from "@/components/SearchBar";
 import { useRouter } from "expo-router";
+import useFetch from "@/services/useFetch";
+import { fetchMovies } from "@/services/api";
 
 export default function Index() {
   const router = useRouter();
+
+  const {data:movies, loading:moviesLoading, error:moviesError} = useFetch(() => fetchMovies({ query: '' }));
 
   return (
     <View className="flex-1 bg-primary">
@@ -15,12 +19,49 @@ export default function Index() {
         <Image source={icons.logo} className="w-12 h-10 mt-20 mb-5 mx-auto" />
       </ScrollView>
 
-      <View className="absolute top-40 w-full px-5">
-        <SearchBar 
-          onPress={() => router.push('/search')}
-          placeholder="Search for movies"
+      {moviesLoading ? (
+        <ActivityIndicator
+          size="large"
+          color="#ab8bff"
+          className="mt-10 self-center"
         />
-      </View>
+      ) : (moviesError ? (
+        <Text className="text-white text-center mt-10">
+          {moviesError?.message}
+        </Text>
+      ) : (
+          <View className="absolute top-40 w-full px-5">
+          <SearchBar 
+            onPress={() => router.push('/search')}
+            placeholder="Search for movies"
+          />
+
+          <Text className="text-lg text-white font-bold mt-5 mb-3">
+            Latest Movies
+          </Text>
+
+          <FlatList
+            data={movies}
+            renderItem={({ item }) => (
+              <Text className="text-white text-sm">
+                {item.title}
+              </Text>
+            )}
+            keyExtractor={(item) => item.id.toString()}
+            numColumns={3}
+            columnWrapperStyle={{ 
+              justifyContent: 'flex-start', 
+              gap: 10,
+              paddingRight: 5,
+              marginBottom: 10
+             }}
+            className="mt-2 pb-32"
+            scrollEnabled={false}
+          />
+        </View>
+      ))}
+
+      
 
     </View>
   );
