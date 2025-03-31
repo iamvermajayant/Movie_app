a02d1fbda73e8c2cc94a2ab56ae9e5b6a51bd3f1
Jayant Verma <iamvermajayant@gmail.com>
2025-03-31 20:46:07 +0530

    feat: added stars and year in the movie card

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -10,7 +10,7 @@ import MovieCard from "@/components/MovieCard";
 export default function Index() {
   const router = useRouter();
 
-  const {data:movies, loading:moviesLoading, error:moviesError} = useFetch(() => fetchMovies({ query: '' }));
+  const { data: movies, loading: moviesLoading, error: moviesError } = useFetch(() => fetchMovies({ query: '' }));
 
   return (
     <View className="flex-1 bg-primary">
@@ -18,49 +18,49 @@ export default function Index() {
 
       <ScrollView className="flex-1 px-5" showsVerticalScrollIndicator={false} contentContainerStyle={{ paddingBottom: 10 }}>
         <Image source={icons.logo} className="w-12 h-10 mt-20 mb-5 mx-auto" />
-      
 
-      {moviesLoading ? (
-        <ActivityIndicator
-          size="large"
-          color="#ab8bff"
-          className="mt-10 self-center"
-        />
-      ) : (moviesError ? (
-        <Text className="text-white text-center mt-10">
-          {moviesError?.message}
-        </Text>
-      ) : (
-          <>
-          <SearchBar 
-            onPress={() => router.push('/search')}
-            placeholder="Search for movies"
+
+        {moviesLoading ? (
+          <ActivityIndicator
+            size="large"
+            color="#ab8bff"
+            className="mt-10 self-center"
           />
-
-          <Text className="text-lg text-white font-bold mt-5 mb-3">
-            Latest Movies
+        ) : (moviesError ? (
+          <Text className="text-white text-center mt-10">
+            {moviesError?.message}
           </Text>
+        ) : (
+          <View className="flex-1">
+            <SearchBar
+              onPress={() => router.push('/search')}
+              placeholder="Search for movies"
+            />
 
-          <FlatList
-            data={movies}
-            renderItem={({ item }) => (
-              <MovieCard
-                {...item}
-              />
-            )}
-            keyExtractor={(item) => item.id.toString()}
-            numColumns={3}
-            columnWrapperStyle={{ 
-              justifyContent: 'flex-start', 
-              gap: 10,
-              paddingRight: 5,
-              marginBottom: 10
-             }}
-            className="mt-2 pb-32"
-            scrollEnabled={false}
-          />
-        </>
-      ))}
+            <Text className="text-lg text-white font-bold mt-5 mb-3 ml-5">
+              Latest Movies
+            </Text>
+
+            <FlatList
+              data={movies}
+              renderItem={({ item }) => (
+                <MovieCard
+                  {...item}
+                />
+              )}
+              keyExtractor={(item) => item.id.toString()}
+              numColumns={3}
+              columnWrapperStyle={{
+                justifyContent: 'flex-start',
+                gap: 10,
+                paddingRight: 5,
+                marginBottom: 10
+              }}
+              className="mt-2 pb-32 ml-5"
+              scrollEnabled={false}
+            />
+          </View>
+        ))}
       </ScrollView>
     </View>
   );
