cc16d362b4a93ceee6126b0694fddd6295ce07cf
jayant <iamvermajayant@gmail.com>
2025-04-06 16:28:34 +0530

    feat: adding the view movie screen

--- services/api.ts
+++ services/api.ts
@@ -12,8 +12,6 @@ export const fetchMovies = async ({ query }: { query: string }) => {
     ? `${TMDB_CONFIG.BASE_URL}/search/movie?query=${encodeURIComponent(query)}`
     : `${TMDB_CONFIG.BASE_URL}/discover/movie?sort_by=popularity.desc`;
 
-    
-
   const response = await fetch(endpoint, {
     method: "GET",
     headers: TMDB_CONFIG.headers,
@@ -29,4 +27,25 @@ export const fetchMovies = async ({ query }: { query: string }) => {
   return data.results;
 };
 
+export const fetchMovieDetails = async (
+  movieId: string
+): Promise<MovieDetails> => {
+  try {
+    const response = await fetch(
+      `${TMDB_CONFIG.BASE_URL}/movie/${movieId}?api_key=${TMDB_CONFIG.API_KEY}`,
+      {
+        method: "GET",
+        headers: TMDB_CONFIG.headers,
+      }
+    );
+    if (!response.ok) {
+      throw new Error(`Error fetching movie details: ${response.statusText}`);
+    }
 
+    const data = await response.json();
+    return data as MovieDetails;
+  } catch (error) {
+    console.log("Error fetching movie details:", error);
+    throw error;
+  }
+};
